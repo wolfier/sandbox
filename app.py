4582116c7f1a120cc3e3b97dcec3bc9009db3f33
root <alanma0907@utexas.edu>
2016-11-06 19:50:48 +0000

    restructure

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, make_response, url_for, send_file
 from flask import Blueprint
+import config
 import os
-import config 
 
 app = Flask(__name__)
 
@@ -10,14 +10,15 @@ if config.DEBUG:
 else:
 	app.config['SERVER_NAME'] = 'wolfier.com'
 
+script_dir = os.path.dirname(__file__)
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
     # Swtich to send_file for production
     # return send_file('templates/index.html')
     # Prevent cachcing for developement testing
-    script_dir = os.path.dirname(__file__)
-    rel_path = "templates/index.html"
+    rel_path = "app/templates/index.html"
     return make_response(open(os.path.join(script_dir, rel_path)).read())
 
 # Blueprint declaration
@@ -26,9 +27,10 @@ bp = Blueprint('subdomain', __name__, subdomain="blog")
 @bp.route('/', defaults={'path': ''})
 @bp.route('/<path:path>')
 def blog(path):
-    return "setting up the blog subdomain"
+    rel_path = "blog/index.html"
+    return make_response(open(os.path.join(script_dir, rel_path)).read())
 
 app.register_blueprint(bp)
 
 if __name__ == "__main__":
-        app.run()
+    app.run()
